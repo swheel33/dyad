007299be4b2460766f397ab5d7a3499f7513af49
Genmin <90125084+Genmin@users.noreply.github.com>
2023-07-04 10:14:45 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -90,10 +90,7 @@ export default function Home() {
             <p className="text-sm leading-loose my-4 px-4">
               They are ERC 721 NFTs with a unique and maximally composable metadata structure. 
             </p>
-           
-        
-         
-
+          
          
             <Table className="p-4">
               <TableHeader>
