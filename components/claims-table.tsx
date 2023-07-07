e2a66f247ef45d137f2e49e9f28acab77b79102f
omurovec <owenmurovec@gmail.com>
2023-07-07 15:15:07 -0700

    feat: add pagnation

--- components/claims-table.tsx
+++ components/claims-table.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { useState } from "react";
+import { formatEther } from "viem";
+
 import { useAllDnftMintsQuery } from "@/gql";
 import {
   Table,
@@ -8,35 +11,77 @@ import {
   TableHead,
   TableHeader,
   TableRow,
+  TableFooter,
 } from "@/components/ui/table";
-import { formatEther } from "viem";
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuLink,
+} from "@/components/ui/navigation-menu";
 import { shortAddr } from "@/lib/utils";
 
 interface Props {
   className?: string;
 }
 
+const perPage = 25;
+
 export default function ClaimsTable({ className }: Props) {
-  const [{ data }] = useAllDnftMintsQuery();
+  const [page, setPage] = useState(0);
+
+  const [{ data }] = useAllDnftMintsQuery({
+    variables: {
+      numResults: perPage,
+      skip: page * perPage,
+    },
+  });
 
   return (
-    <Table className={className}>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[200px]">User</TableHead>
-          <TableHead>dNFT ID</TableHead>
-          <TableHead>Contribution</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {data?.mintedNfts.map((mint) => (
-          <TableRow key={mint.DNft_id}>
-            <TableCell className="font-medium">{shortAddr(mint.to)}</TableCell>
-            <TableCell>#{+mint.DNft_id + 1}</TableCell>
-            <TableCell>{formatEther(mint.price)} ETH</TableCell>
+    <div className={className}>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[200px]">User</TableHead>
+            <TableHead>dNFT ID</TableHead>
+            <TableHead>Contribution</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {data?.mintedNfts.map((mint) => (
+            <TableRow key={mint.DNft_id}>
+              <TableCell className="font-medium">
+                {shortAddr(mint.to)}
+              </TableCell>
+              <TableCell>#{+mint.DNft_id + 1}</TableCell>
+              <TableCell>{formatEther(mint.price)} ETH</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <div className="w-full max-w-none flex justify-center align-center mt-4 gap-8">
+        <div className="w-8 text-center">
+          {page !== 0 && (
+            <p
+              className="text-md text-muted-foreground hover:text-foreground cursor-pointer"
+              onClick={() => setPage(page - 1)}
+            >
+              prev
+            </p>
+          )}
+        </div>
+        <p className="text-md text-center">{page + 1}</p>
+        <div className="w-8 text-center">
+          {data?.mintedNfts.length === perPage && (
+            <p
+              className="text-md text-muted-foreground hover:text-foreground cursor-pointer justify-self-end"
+              onClick={() => setPage(page + 1)}
+            >
+              next
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
