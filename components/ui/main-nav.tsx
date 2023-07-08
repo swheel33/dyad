2d266ca744cd8122a605c33848ac6a19027e3b97
Genmin <90125084+Genmin@users.noreply.github.com>
2023-07-07 22:57:57 -0700

    Update main-nav.tsx

--- components/ui/main-nav.tsx
+++ components/ui/main-nav.tsx
@@ -12,25 +12,25 @@ export function MainNav({
       {...props}
     >
       <Link
-        href="/"
+        href="https://dyadstablecoin.github.io/claim-frontend/"
         className="text-sm font-medium transition-colors hover:text-primary"
       >
         DYAD
       </Link>
       <Link
-        href="/examples/dashboard"
+        href="https://dyadstable.notion.site/DYAD-full-52096aed265247e7a50b14f06c228a7e?pvs=4"
         className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
       >
         Docs
       </Link>
       <Link
-        href="/examples/dashboard"
+        href="https://discord.gg/tBtWErKQ"
         className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
       >
         Discord
       </Link>
       <Link
-        href="/examples/dashboard"
+        href="https://twitter.com/0xDYAD"
         className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
       >
         Twitter
