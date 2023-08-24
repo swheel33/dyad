b66144ef2bc5a2c06dc6a2ed6d6d7371aebd7476
omurovec <owenmurovec@gmail.com>
2023-08-24 11:56:50 -0700

    fix dnft modal ui

--- components/dnft-modal-content.tsx
+++ components/dnft-modal-content.tsx
@@ -16,6 +16,8 @@ import {
 import DnftChart from "./dnft-chart";
 import { CardTitle } from "./ui/card";
 import { Separator } from "@/components/ui/separator";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Label } from "@/components/ui/label";
 
 const testData = [
   {
@@ -55,6 +57,27 @@ const testData = [
   },
 ];
 
+const vaults = [
+  {
+    id: "wETH",
+    value: 16700,
+    apy: 5.6,
+    earnings: 897,
+  },
+  {
+    id: "stETH",
+    value: 8030,
+    apy: 3.7,
+    earnings: 277,
+  },
+  {
+    id: "TBY",
+    value: 6852,
+    apy: 8.0,
+    earnings: 683,
+  },
+];
+
 interface Props {
   dnft: {
     id: string;
@@ -79,12 +102,12 @@ export default function DnftModalContent({ dnft }: Props) {
 
         <div className="mint-section flex space-x-2 pt-4">
           <Input type="number" placeholder="Mint DYAD" />
-          <Button>MAX</Button>
+          <Button variant="secondary">MAX</Button>
         </div>
 
         <div className="redeem-section flex space-x-2">
           <Input type="number" placeholder="Redeem DYAD" />
-          <Button>MAX</Button>
+          <Button variant="secondary">MAX</Button>
         </div>
       </div>
 
@@ -96,54 +119,72 @@ export default function DnftModalContent({ dnft }: Props) {
               <TableHead>Value</TableHead>
               <TableHead>Yield</TableHead>
               <TableHead>Earnings</TableHead>
-              <TableHead>Actions</TableHead>
+              <TableHead className="w-20">Actions</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow>
-              <TableCell>1</TableCell>
-              <TableCell>1</TableCell>
-              <TableCell>1</TableCell>
-              <TableCell>1</TableCell>
-              <TableCell>1</TableCell>
-            </TableRow>
+            {vaults.map(({ id, value, apy, earnings }) => (
+              <TableRow key={id}>
+                <TableCell>{id}</TableCell>
+                <TableCell>${value}</TableCell>
+                <TableCell>{apy}%</TableCell>
+                <TableCell>${earnings}</TableCell>
+                <TableCell>
+                  <Popover>
+                    <PopoverTrigger>
+                      <Button variant="outline">±</Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-80">
+                      <RadioGroup defaultValue="deposit" className="space-y-2">
+                        <div className="flex items-center space-x-2">
+                          <RadioGroupItem value="deposit" id="deposit" />
+                          <Label htmlFor="deposit" className="cursor-pointer">
+                            Deposit
+                          </Label>
+                        </div>
+                        <div className="flex space-x-2">
+                          <Input type="number" placeholder="Deposit" />
+                          <Button variant="secondary">MAX</Button>
+                        </div>
+                        <div className="flex items-center space-x-2 cursor-pointer">
+                          <RadioGroupItem value="withdraw" id="withdraw" />
+                          <Label htmlFor="withdraw" className="cursor-pointer">
+                            Withdraw
+                          </Label>
+                        </div>
+                        <div className="flex space-x-2">
+                          <Input type="number" placeholder="Withdraw" />
+                          <Button variant="secondary">MAX</Button>
+                        </div>
+                        <div className="pt-2 w-full">
+                          <Button className="w-full">CONFIRM</Button>
+                        </div>
+                      </RadioGroup>
+                    </PopoverContent>
+                  </Popover>
+                </TableCell>
+              </TableRow>
+            ))}
+            {new Array(5 - vaults.length).fill(null).map((_, idx) => (
+              <TableRow key={`option-${idx}`}>
+                <TableCell />
+                <TableCell />
+                <TableCell />
+                <TableCell />
+                <TableCell>
+                  <Popover>
+                    <PopoverTrigger>
+                      <Button variant="outline">+</Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-80">
+                      {/* ... (Your provided design for the Deposit/Withdraw popover) ... */}
+                    </PopoverContent>
+                  </Popover>
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
-
-        {[{ id: "test" }].map((vault) => (
-          <div key={vault.id} className="flex justify-between py-2 border-t">
-            <div className="flex space-x-2">
-              <Popover>
-                <PopoverTrigger>
-                  <Button variant="outline">±</Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-80">
-                  {/* ... (Your provided design for the Deposit/Withdraw popover) ... */}
-                </PopoverContent>
-              </Popover>
-            </div>
-          </div>
-        ))}
-
-        {Array(5)
-          .fill(null)
-          .map((_, idx) => (
-            <div
-              key={`empty-${idx}`}
-              className="flex justify-center py-2 border-t"
-            >
-              <Popover>
-                <PopoverTrigger>
-                  <Button variant="outline">+</Button>
-                </PopoverTrigger>
-                <PopoverContent>
-                  {[{ id: "test", name: "test" }].map((vault) => (
-                    <Button key={vault.id}>{vault.name}</Button>
-                  ))}
-                </PopoverContent>
-              </Popover>
-            </div>
-          ))}
         <div className="claim-section">
           <Button>Claim Bonus</Button>
         </div>
