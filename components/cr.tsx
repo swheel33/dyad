3b1eba3d0455bb60dc5af78c3ad72fe892355042
shafu.eth <selfouly@gmail.com>
2023-12-13 19:45:38 +0100

    change color

--- components/cr.tsx
+++ components/cr.tsx
@@ -1,21 +1,24 @@
 import { round } from "../utils/currency";
+import { useState, useEffect } from "react";
 
 export default function CR({ cr }) {
-  let textColor = "white";
+  const [textColor, setTextColor] = useState("white");
 
-  if (cr < 150) {
-    textColor = "#FF0000";
-  } else if (cr < 200) {
-    textColor = "#FF8C00";
-  } else if (cr < 225) {
-    textColor = "FFA500";
-  } else if (cr < 250) {
-    textColor = "FFFF00";
-  } else if (cr < 275) {
-    textColor = "90EE90";
-  } else {
-    textColor = "008000";
-  }
+  useEffect(() => {
+    if (cr < 150) {
+      setTextColor("#FF0000");
+    } else if (cr < 200) {
+      setTextColor("#FF8C00");
+    } else if (cr < 225) {
+      setTextColor("#FFA500");
+    } else if (cr < 250) {
+      setTextColor("#FFFF00");
+    } else if (cr < 275) {
+      setTextColor("#90EE90");
+    } else {
+      setTextColor("#008000");
+    }
+  }, [cr]);
 
   return <span style={{ color: textColor }}>{round(cr, 2)}</span>;
 }
