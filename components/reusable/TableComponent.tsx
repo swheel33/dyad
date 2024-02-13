4946445212023b99aea6b6ea10af75e574dc2ef7
Mostafa Mahmoud <mostafamahmoud24@gmail.com>
2024-02-13 23:04:12 +0200

    styling enhancements

--- components/reusable/TableComponent.tsx
+++ components/reusable/TableComponent.tsx
@@ -7,26 +7,32 @@ import {
   getKeyValue,
   Table,
 } from "@nextui-org/react";
-// import { useInfiniteScroll } from "@nextui-org/use-infinite-scroll";
 import React from "react";
 
 interface TableComponentProps {
   columns: any;
   rows: any;
+  size?: "default" | "compact";
+  onRowClick?: (key: string | number) => void;
 }
 
-const TableComponent: React.FC<TableComponentProps> = ({ columns, rows }) => {
-  //   const [scrollerRef] = useInfiniteScroll({});
+const TableComponent: React.FC<TableComponentProps> = ({
+  columns,
+  rows,
+  onRowClick,
+  size = "default",
+}) => {
   return (
     <Table
+      aria-label="Table"
       removeWrapper
-      //   isHeaderSticky
       shadow="none"
-      //   baseRef={scrollerRef}
       classNames={{
         th: " table-header",
         tbody: "px-0",
-        tr: "table-row",
+        tr: `${onRowClick ? "cursor-pointer hover:text-[#FAFAFA]" : ""} ${
+          size === "compact" ? "h-[35px]" : ""
+        } table-row`,
         td: "px-0",
       }}
     >
@@ -37,7 +43,10 @@ const TableComponent: React.FC<TableComponentProps> = ({ columns, rows }) => {
       </TableHeader>
       <TableBody items={rows}>
         {(item: any) => (
-          <TableRow key={item.key}>
+          <TableRow
+            key={item.key}
+            onClick={onRowClick ? () => onRowClick(item.key) : () => {}}
+          >
             {(columnKey) => (
               <TableCell>{getKeyValue(item, columnKey)}</TableCell>
             )}
